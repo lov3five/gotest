a873f127d358c647e6865b38b3ff7659fc94e0d4
lov3five <luongtranstaff@gmail.com>
2023-10-26 10:53:18 +0700

    Issue name

--- gotest.go
+++ gotest.go
@@ -3,6 +3,7 @@ package gotest
 import "fmt"
 
 func Hello(name string) string {
-	message := fmt.Sprintf("Hi, %v. Welcome to Golang!!!")
+	message := fmt.Sprintf("Hi, %v. Welcome to Golang!!!", name)
 	return message
 }
+
